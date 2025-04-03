d4f304e561ef9ea8da10f428c550a1e41b56aa36
a4001234567 <48621584+a4001234567@users.noreply.github.com>
2025-04-03 15:22:41 +0800

    Update funcPhi.py
    Now funcPhi.py can be used in PyTorch.

--- utils/funcPhi.py
+++ utils/funcPhi.py
@@ -1,7 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.integrate import quad
-
 from numba import jit
 
 # Define the integrand function
@@ -12,7 +10,7 @@ def integrand(t, mu):
     Here for numeric precision, we shift by mu to make the center of the gaussian at 0
     This is consistent with scipy.integrate.quad which for infinite integration, centering at 0.
     '''
-    return np.tanh(.5*(t+mu)) * np.exp(- np.pow(t,2) / (4 * mu))
+    return np.tanh(.5*(t+mu)) * np.exp(- np.power(t,2) / (4 * mu))
 
 # Define the function f(mu)
 def f(mu):
@@ -21,7 +19,7 @@ def f(mu):
     return 1 - (1 / np.sqrt(4 * np.pi * mu)) * integral_value
 
 # Generate an array of mu values
-mu_values = np.logspace(-4, 4, 100)
+mu_values = np.logspace(-6, 6, 10000)
 f_values = np.clip(np.array([f(mu) for mu in mu_values]),a_min=0,a_max=np.inf)
 
 def generate_func_from_points(x_values,fx_values):
@@ -35,15 +33,67 @@ def generate_func_from_points(x_values,fx_values):
 _func_phi = generate_func_from_points(mu_values,f_values)
 def func_phi(mu):
     return _func_phi(mu)
+
 _func_phi_inv = generate_func_from_points(f_values,mu_values)
 def func_phi_inv(mu):
     return _func_phi_inv(mu)
 
+try:
+    import torch
+
+    indices = np.argsort(mu_values)
+    x_sorted = mu_values[indices]
+    f_sorted = f_values[indices]
+    _X, unique_indices = np.unique(x_sorted, return_index=True)
+    _Y = f_sorted[unique_indices]
+    _dX = .5 * (_X[1:] + _X[:-1])
+    _dY = (_Y[1:] - _Y[:-1]) / (_X[1:] - _X[:-1])
+
+    inv_indices = np.argsort(f_values)
+    x_sorted_inv = f_values[inv_indices]
+    f_sorted_inv = mu_values[inv_indices]
+    _X_inv, unique_indices_inv = np.unique(x_sorted_inv, return_index=True)
+    _Y_inv = f_sorted_inv[unique_indices_inv]
+    _dX_inv = .5 * (_X_inv[1:] + _X_inv[:-1])
+    _dY_inv = (_Y_inv[1:] - _Y_inv[:-1]) / (_X_inv[1:] - _X_inv[:-1])
+
+    class funcPhiTorch(torch.autograd.Function):
+        @staticmethod
+        def forward(ctx, x):
+            ctx.save_for_backward(x)
+            return torch.tensor(np.interp(np.array(x), _X, _Y))
+        @staticmethod
+        def backward(ctx, grad_input):
+            input_x, = ctx.saved_tensors
+            return torch.tensor(np.interp(np.array(input_x), _dX, _dY)) * grad_input
+
+    class funcPhiInvTorch(torch.autograd.Function):
+        @staticmethod
+        def forward(ctx, x):
+            ctx.save_for_backward(x)
+            return torch.tensor(np.interp(np.array(x), _X_inv, _Y_inv))
+        @staticmethod
+        def backward(ctx, grad_input):
+            input_x, = ctx.saved_tensors
+            return torch.tensor(np.interp(np.array(input_x), _dX_inv, _dY_inv)) * grad_input
+
+except ImportError:
+    class funcPhiTorch:
+        raise NotImplementedError("Torch is not available, funcPhiTorch cannot be used.")
+    class funcPhiInvTorch:
+        raise NotImplementedError("Torch is not available, funcPhiInvTorch cannot be used.")
+    
+
 if __name__ == '__main__':
-    #print(f(100000))
-    #exit()
-    #plt.plot(mu_values, f_values)
-    #plt.show()
+    eps = 1e-5
+    xs = np.abs(np.random.randn(5))
+    assert np.all(np.abs(funcPhiTorch.apply(torch.tensor(xs)).numpy()-func_phi(xs)) <= eps)
+    assert np.all(np.abs(funcPhiInvTorch.apply(torch.tensor(xs)).numpy()-func_phi_inv(xs)) <= eps)
+    xs = torch.tensor(xs,dtype=torch.double, requires_grad=True)
+    assert torch.autograd.gradcheck(funcPhiTorch.apply, (xs,),rtol=1e-2,atol=1e-2)
+    xs = torch.rand(5,dtype=torch.double, requires_grad=True)
+    assert torch.autograd.gradcheck(funcPhiInvTorch.apply, (xs,),rtol=1e-2,atol=1e-2)
+    exit()
     xs = np.linspace(0,1,1000)
     plt.figure(figsize=(8, 6))
     plt.plot(xs, func_phi(xs), label=r'$f(\mu)$', color='b')
